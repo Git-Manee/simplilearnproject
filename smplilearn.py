e2885abb6d7b21a4c5c798fe141a192cffb203f4
Mani <manesorapalli@gmail.com>
2023-06-30 19:16:04 +0530

    Caltech boot camp welcome msg

---
+++ smplilearn.py
@@ -0,0 +1,10 @@
+# Caltech Boot Camp Welcome Message
+
+def welcome_message():
+    print("Welcome to Caltech boot camp!")
+
+def main():
+    welcome_message()
+
+if __name__ == "__main__":
+    main()
